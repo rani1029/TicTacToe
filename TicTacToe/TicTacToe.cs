1806a2419555d4156002836169c075edec78093b
rani1029 <ranig1029@gmail.com>
2021-09-01 09:51:24 +0530

    Empty board is created

---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToe
+    {
+        public static void CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i <= 10; i++)
+            {
+                board[i] = ' ';
+            }
+
+        }
+
+
+    }
+}
